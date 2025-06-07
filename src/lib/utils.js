9232a87a64f1a3f8562899815f241744641229eb
shashank-iter <pandeyshashank812@gmail.com>
2025-06-08 02:22:18 +0530

    feat: add components and utils

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -12,3 +12,11 @@ export function formatDate(dateString) {
     year: "numeric",
   }).format(date)
 }
+
+export function convertToDatePickerFormat(inputDate) {
+  const date = new Date(inputDate);
+  const yyyy = date.getUTCFullYear();
+  const mm = String(date.getUTCMonth() + 1).padStart(2, '0');
+  const dd = String(date.getUTCDate()).padStart(2, '0');
+  return `${yyyy}-${mm}-${dd}`;
+}
